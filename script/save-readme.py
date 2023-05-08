dd5626b98d2b34a3832ccd713d0ec2c8b3eec079
JeongMi <76805879+jung0115@users.noreply.github.com>
2023-05-09 04:59:03 +0900

    메인 readme 값 복사

--- script/save-readme.py
+++ script/save-readme.py
@@ -1,9 +1,17 @@
 import os
+from os import path
+import shutil
 
-readme_file_path = "../save/README.md"
+origin_readme_file_path = "../README.md"
+save_readme_file_path = "../save/README.md"
+
+# 폴더 없으면 생성
 if not os.path.exists("../save"):
   os.makedirs("../save")
 
-readme = open(readme_file_path, "w")
-readme.write("# Test")
-readme.close()
+#readme = open(readme_file_path, "w")
+#readme.write("# Test")
+#readme.close()
+
+# readme 내용 copy
+shutil.copy(origin_readme_file_path, save_readme_file_path)
